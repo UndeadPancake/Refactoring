b680723505c9a9b5c88423c3dcadaf96b5c67a27
Martin Johansson <maxamartin1999@gmail.com>
2020-11-13 13:56:47 +0100

    Updated variable names

--- Inlamning_2_ra_kod/Program.cs
+++ Inlamning_2_ra_kod/Program.cs
@@ -27,9 +27,7 @@ namespace Inlamning_2_ra_kod
                 while (fileStream.Peek() >= 0)
                 {
                     string line = fileStream.ReadLine();
-                    // Console.WriteLine(line);
                     string[] word = line.Split('#');
-                    // Console.WriteLine("{0}, {1}, {2}, {3}", word[0], word[1], word[2], word[3]);
                     Person P = new Person(word[0], word[1], word[2], word[3]);
                     Dict.Add(P);
                 }
@@ -55,23 +53,23 @@ namespace Inlamning_2_ra_kod
                     Console.Write("  2. ange adress:  ");
                     string adress = Console.ReadLine();
                     Console.Write("  3. ange telefon: ");
-                    string telefon = Console.ReadLine();
+                    string phone = Console.ReadLine();
                     Console.Write("  4. ange email:   ");
                     string email = Console.ReadLine();
-                    Dict.Add(new Person(name, adress, telefon, email));
+                    Dict.Add(new Person(name, adress, phone, email));
                 }
                 else if (command == "ta bort")
                 {
                     Console.Write("Vem vill du ta bort (ange namn): ");
-                    string villTaBort = Console.ReadLine();
+                    string toBeRemoved = Console.ReadLine();
                     int found = -1;
                     for (int i = 0; i < Dict.Count(); i++)
                     {
-                        if (Dict[i].name == villTaBort) found = i;
+                        if (Dict[i].name == toBeRemoved) found = i;
                     }
                     if (found == -1)
                     {
-                        Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", villTaBort);
+                        Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", toBeRemoved);
                     }
                     else
                     {
@@ -89,28 +87,28 @@ namespace Inlamning_2_ra_kod
                 else if (command == "ändra")
                 {
                     Console.Write("Vem vill du ändra (ange namn): ");
-                    string villÄndra = Console.ReadLine();
+                    string personChange = Console.ReadLine();
                     int found = -1;
                     for (int i = 0; i < Dict.Count(); i++)
                     {
-                        if (Dict[i].name == villÄndra) found = i;
+                        if (Dict[i].name == personChange) found = i;
                     }
                     if (found == -1)
                     {
-                        Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", villÄndra);
+                        Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", personChange);
                     }
                     else
                     {
                         Console.Write("Vad vill du ändra (namn, adress, telefon eller email): ");
-                        string fältAttÄndra = Console.ReadLine();
-                        Console.Write("Vad vill du ändra {0} på {1} till: ", fältAttÄndra, villÄndra);
-                        string nyttVärde = Console.ReadLine();
-                        switch (fältAttÄndra)
+                        string elementToChange = Console.ReadLine();
+                        Console.Write("Vad vill du ändra {0} på {1} till: ", elementToChange, personChange);
+                        string newValue = Console.ReadLine();
+                        switch (elementToChange)
                         {
-                            case "namn": Dict[found].name = nyttVärde; break;
-                            case "adress": Dict[found].adress = nyttVärde; break;
-                            case "telefon": Dict[found].phone = nyttVärde; break;
-                            case "email": Dict[found].email = nyttVärde; break;
+                            case "namn": Dict[found].name = newValue; break;
+                            case "adress": Dict[found].adress = newValue; break;
+                            case "telefon": Dict[found].phone = newValue; break;
+                            case "email": Dict[found].email = newValue; break;
                             default: break;
                         }
                     }
@@ -120,10 +118,6 @@ namespace Inlamning_2_ra_kod
                     Console.WriteLine("Okänt kommando: {0}", command);
                 }
             } while (command != "sluta");
-        }
-        static void LoadAdresslist()
-        {
-
         }
     }
 }
